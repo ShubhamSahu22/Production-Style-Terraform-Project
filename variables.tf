2d593d22acedf5bf0dfbfae1d30dbd6fd35a354c
Shubham Kumar Sahu <shubhamsahu410@gmail.com>
2025-08-10 00:59:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" { default = "us-east-1" }
-variable "ami_id" { default = "ami-08c40ec9ead489470" }
+variable "ami_id" { default = "ami-020cba7c55df1f615" }
 variable "key_pair" {}
 variable "db_user" { default = "admin" }
 variable "db_pass" {}
