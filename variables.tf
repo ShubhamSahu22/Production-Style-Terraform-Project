88cf33701fcc17d9b654fcd174942d11faadba12
shubham <shubhamsahu410@gmail.com>
2025-08-10 11:50:09 +0530

    1st

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" { default = "us-east-1" }
-variable "ami_id" { default = "ami-020cba7c55df1f615" }
+variable "ami_id" { default = "ami-0de716d6197524dd9" }
 variable "key_pair" {}
 variable "db_user" { default = "admin" }
 variable "db_pass" {}
